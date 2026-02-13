8607f341a1ee84d6f574f240901b5e4fd0145394
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-13 09:15:53 -0300

    feat:adds a component that defines the structure

---
+++ src/pages/CategoryExpenses/index.tsx
@@ -0,0 +1,33 @@
+import BudgetSummary from "./components/BudgetSummary";
+import ExpenseList from "./components/ExpenseList";
+import Header from "./components/Header";
+import SummaryItemCard from "./components/SummaryItemCard";
+
+const CategoryExpenses = () => {
+  return (
+    <section className="relative m-auto flex min-h-dvh w-full max-w-2xl flex-col">
+      <div className="relative h-64">
+        <div
+          className="absolute top-0 left-0 z-0 h-full w-full max-w-2xl bg-gray-900"
+          style={{
+            clipPath: "polygon(0 0, 100% 0, 100% 82%, 0 100%)",
+          }}
+        />
+        <Header categoryName="Alimetação" status="ACIMA" />
+        <BudgetSummary />
+      </div>
+      <div className="flex justify-around gap-4 border-b border-gray-200 py-2">
+        <SummaryItemCard label="Sálario" value={20} />
+        <SummaryItemCard label="Gastos" value={53} />
+        <SummaryItemCard label="Saldo" value={31} />
+      </div>
+
+      <ExpenseList />
+      <button className="rounded-2xl bg-blue-600 px-4 py-2 font-medium text-white">
+        <i className="bi bi-plus" /> Adicionar gasto
+      </button>
+    </section>
+  );
+};
+
+export default CategoryExpenses;
