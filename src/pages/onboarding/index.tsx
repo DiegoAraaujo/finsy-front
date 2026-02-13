4ad560ad9c3baf659a1704cc5a6a56509e996cf6
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-13 09:14:28 -0300

    feat: adds a component that defines the structure

---
+++ src/pages/onboarding/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Onboarding = () => {
+  const [step, setStep] = useState<number>(0);
+
+  return <section className="max-w-2xl w-full m-auto bg-red-500">
+    
+  </section>;
+};
+
+export default Onboarding;
