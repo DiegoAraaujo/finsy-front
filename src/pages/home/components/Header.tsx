0f742e705400aabe2bcd498d2247a64b13bceb07
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-13 08:46:23 -0300

    feat(Header):  add page header

---
+++ src/pages/home/components/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <div className="relative z-10 flex justify-between p-4">
+      <div>
+        <p className="text-sm text-gray-400">Fevereiro de 2026</p>
+        <p className="font-bold text-white">Olá, Diego Araújo! 👋</p>
+      </div>
+
+      <div className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-blue-300 bg-blue-700 text-white">
+        <p>DA</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
