813c49baa3d6f1e2ddf26fb26cee97f5af828fe4
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-18 09:05:23 -0300

    feat: Add category file with initial data.

---
+++ src/data/categories.ts
@@ -0,0 +1,28 @@
+const categories = [
+  {
+    id: 1,
+    name: "Food",
+    totalBudget: 8,
+    spentAmount: 3.55,
+  },
+  {
+    id: 2,
+    name: "Transport",
+    totalBudget: 20,
+    spentAmount: 12.3,
+  },
+  {
+    id: 3,
+    name: "Entertainment",
+    totalBudget: 15,
+    spentAmount: 17.9,
+  },
+  {
+    id: 4,
+    name: "Shopping",
+    totalBudget: 50,
+    spentAmount: 22.75,
+  },
+];
+
+export default categories;
