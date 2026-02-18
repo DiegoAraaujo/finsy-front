066da2c7fb2ffa2fcad3f0c58e6887cdccfa4afa
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-18 09:01:29 -0300

    feat: refactor Home component to include loading states and financial summary display

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,28 +3,51 @@ import BudgetSummary from "./components/BudgetSummary";
 import Header from "./components/Header";
 import SummaryItemCard from "./components/SummaryItemCard";
 import PageHeaderBackground from "../../components/PageHeaderBackground";
+import Loading from "../../components/Loading";
+import useFinances from "../../hooks/useFinances";
+import useCategories from "../../hooks/useCategories";
 
 const Home = () => {
+  const { categories, categoriesLoading, categoriesError } = useCategories();
+  const { financialSummary, financesLoading, financesError } = useFinances();
+
+  if (categoriesLoading || financesLoading) return <Loading />;
+  if (categoriesError || financesError) return <p>Erro ao carregar</p>;
+
   return (
-    <section className="relative m-auto flex min-h-dvh w-full max-w-2xl flex-col">
+    <>
       <div className="relative h-64">
         <PageHeaderBackground />
         <Header />
         <BudgetSummary />
       </div>
 
-      <div className="flex justify-around gap-4 border-b border-gray-200 py-2">
-        <SummaryItemCard label="Sálario" value={20} />
-        <SummaryItemCard label="Gastos" value={53} />
-        <SummaryItemCard label="Saldo" value={31} />
-      </div>
+      {financialSummary && (
+        <div className="flex justify-around gap-4 border-b border-gray-200 py-2">
+          <SummaryItemCard
+            label="Salário"
+            value={financialSummary.salary}
+            textColor="text-gray-900"
+          />
+          <SummaryItemCard
+            label="Gastos"
+            value={financialSummary.expenses}
+            textColor="text-gray-900"
+          />
+          <SummaryItemCard
+            label="Saldo"
+            value={financialSummary.salary - financialSummary.expenses}
+            textColor={
+              financialSummary.salary - financialSummary.expenses >= 0
+                ? "text-emerald-600"
+                : "text-red-600"
+            }
+          />
+        </div>
+      )}
 
-      <CategoriesSection />
-
-      <button className="rounded-2xl bg-blue-600 px-4 py-2 font-medium text-white">
-        <i className="bi bi-plus" /> Adicionar gasto
-      </button>
-    </section>
+      {categories && <CategoriesSection categories={categories} />}
+    </>
   );
 };
 
