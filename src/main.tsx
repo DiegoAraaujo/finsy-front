ac3075e0a04771798fa97d2dc7b1d420fe848244
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-18 08:59:59 -0300

    feat(QueryClient): add QueryClientProvider for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,22 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
