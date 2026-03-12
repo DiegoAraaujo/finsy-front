5876f2f2a5849e576591d16d8f82b5d52f7cd3c2
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-12 09:23:44 -0300

    style(Header): change text color

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="relative z-10 flex justify-between p-4">
       <div>
         <p className="text-sm text-gray-400">Fevereiro de 2026</p>
-        <p className="font-bold text-white">Olá, Diego Araújo! 👋</p>
+        <p className="font-bold">Olá, Diego Araújo! 👋</p>
       </div>
 
       <div className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-blue-300 bg-blue-700 text-white">
