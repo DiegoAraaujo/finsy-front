6813156a0a327a6587ac635248e3d19ac7d6d6f2
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-12 15:45:56 -0300

    feat(UserContext): creates context for the user

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useState, type ReactNode } from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined,
+);
+
+export const UserContextProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
