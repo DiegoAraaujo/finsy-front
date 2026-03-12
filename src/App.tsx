0dee2c63aa025b42b357f9d673642df35114f2e7
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-12 15:49:26 -0300

    feat(app):reorganized routes, protecting them and adding the toast.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+
 import Home from "./pages/home";
 import BudgetPlanningPage from "./pages/budgetPlanningPage";
 import Onboarding from "./pages/onboarding";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Historic from "./pages/historic";
 import CategoryExpenses from "./pages/CategoryExpenses";
 import Container from "./components/Container";
+import Login from "./pages/login";
+import { UserContextProvider } from "./contexts/UserContext";
+import ProtectedLayout from "./components/ProtectedLayout";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:id" element={<CategoryExpenses />} />
-          <Route path="/historic" element={<Historic />} />
-          <Route path="/budget-planning" element={<BudgetPlanningPage />} />
-        </Routes>
-      </Container>
-      {/* <Onboarding /> */}
-    </BrowserRouter>
+    <UserContextProvider>
+      <BrowserRouter>
+        <Toaster richColors position="top-right" closeButton duration={3000} />
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedLayout>
+                  <Home />
+                </ProtectedLayout>
+              }
+            />
+            <Route
+              path="/historic"
+              element={
+                <ProtectedLayout>
+                  <Historic />
+                </ProtectedLayout>
+              }
+            />
+            <Route
+              path="/budget-planning"
+              element={
+                <ProtectedLayout>
+                  <BudgetPlanningPage />
+                </ProtectedLayout>
+              }
+            />
+            <Route
+              path="/category/:id"
+              element={
+                <ProtectedLayout>
+                  <CategoryExpenses />
+                </ProtectedLayout>
+              }
+            />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </UserContextProvider>
   );
 };
 
