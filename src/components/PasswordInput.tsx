263c59ee569e8d9ebed1e6fd56665a32955a11ae
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-12 15:48:21 -0300

    feat: Creates input fields for passwords and text.

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+interface PasswordInputProps {
+  id: string;
+  label: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  value: string;
+}
+const PasswordInput = ({
+  id,
+  label,
+  onChange,
+  value,
+  placeholder,
+}: PasswordInputProps) => {
+  const [visiblePassword, setVisiblePassword] = useState<boolean>(false);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  return (
+    <div>
+      <label htmlFor={id} className="text-gray-500">
+        {label}
+      </label>
+      <div
+        className={`${isFocused ? "border-blue-400" : "border-gray-200"} flex gap-4 rounded-2xl border bg-white p-2`}
+      >
+        <i className="bi bi-lock text-blue-400"></i>
+        <input
+          onChange={(e) => onChange(e.target.value)}
+          value={value}
+          type={visiblePassword ? "text" : "password"}
+          id={id}
+          className="flex-1 text-sm focus:outline-0"
+          placeholder={placeholder}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+        <button
+          type="button"
+          className="cursor-pointer"
+          onClick={() => setVisiblePassword(!visiblePassword)}
+        >
+          <i
+            className={`${visiblePassword ? "bi bi-eye-slash" : "bi bi-eye"} text-gray-500`}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordInput;
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+interface TextInputProps {
+  label: string;
+  type?: string;
+  id: string;
+  placeholder?: string;
+  icon?: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const TextInput = ({
+  label,
+  type = "text",
+  id,
+  placeholder,
+  icon,
+  onChange,
+  value,
+}: TextInputProps) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  return (
+    <div>
+      <label htmlFor={id} className="text-gray-500">
+        {label}
+      </label>
+
+      <div
+        className={`flex gap-4 rounded-2xl border ${isFocused ? "border-blue-400" : "border-gray-200"} bg-white p-2`}
+      >
+        {icon && <i className={`bi ${icon} text-blue-400`} />}
+        <input
+          value={value}
+          type={type}
+          onChange={(e) => onChange(e.target.value)}
+          id={id}
+          className="flex-1 text-sm focus:outline-0"
+          placeholder={placeholder}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TextInput;
