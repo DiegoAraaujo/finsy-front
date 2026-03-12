d8ca0ee504725e8c4171ffac2e93e0c5099667c1
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-12 15:43:40 -0300

    style: adjust container measurements

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,9 +6,9 @@ interface ContainerProps {
 
 const Container = ({ children }: ContainerProps) => {
   return (
-    <main className="relative m-auto h-dvh min-h-dvh w-full max-w-2xl overflow-y-auto">
+    <main className="relative m-auto h-dvh min-h-dvh w-full max-w-2xl overflow-y-auto overflow-hidden p-4">
       <PageHeaderBackground />
-      <div className="relative z-20">{children}</div>
+      <div className="relative z-20 h-full">{children}</div>
     </main>
   );
 };
