284b89fd2d79bdc647931bd14334bd3200aca648
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-13 07:51:53 -0300

    feat(api): adds the access token to the header

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+import { getAccessToken } from "../utils/auth";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${getAccessToken()}`,
+  },
 });
 
 export default api;
