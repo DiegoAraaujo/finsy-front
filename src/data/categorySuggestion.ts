0123e2bb1fb25ae0009d85943b5661c77f28d477
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-17 08:34:10 -0300

    feat(CategorySuggestion): Mocked data for category suggestions

---
+++ src/data/categorySuggestion.ts
@@ -0,0 +1,14 @@
+export const categorySuggestions = [
+  "Alimentação",
+  "Moradia",
+  "Transporte",
+  "Lazer",
+  "Saúde",
+  "Educação",
+  "Compras",
+  "Assinaturas",
+  "Contas",
+  "Investimentos",
+  "Emergência",
+  "Outros",
+];
