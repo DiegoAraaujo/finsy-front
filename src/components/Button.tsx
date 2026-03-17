89034648d4f5e5ff474093fdc4e4362ad2f50137
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-17 08:15:01 -0300

    feat(Button): Changes behavior when disabled.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,29 @@
 interface ButtonProps {
   label: string;
   loadingLabel?: string;
-  loading: boolean;
+  loading?: boolean;
+  disabled?: boolean;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
 }
+
 const Button = ({
   label,
   type = "button",
   onClick,
-  loading,
+  loading = false,
+  disabled = false,
   loadingLabel = "carregando...",
 }: ButtonProps) => {
+  const isDisabled = loading || disabled;
+
   return (
     <button
-      disabled={loading}
+      disabled={isDisabled}
       type={type}
       onClick={onClick}
       aria-busy={loading}
-      className={`w-full cursor-pointer rounded-2xl bg-blue-600 p-2 font-semibold text-white transition-all duration-300 hover:-translate-y-0.5 hover:shadow-md disabled:transform-none disabled:opacity-60`}
+      className="w-full cursor-pointer rounded-2xl bg-blue-600 p-2 font-semibold text-white transition-all duration-300 hover:-translate-y-0.5 hover:shadow-md disabled:pointer-events-none disabled:transform-none disabled:opacity-60"
     >
       {loading ? loadingLabel : label}
     </button>
