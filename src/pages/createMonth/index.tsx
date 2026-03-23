d6bf688a884e4c58612b9a7c606ea0da51e49b3a
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-23 20:19:59 -0300

    refactor: extract handleCreateMonth logic into a dedicated function within CreateMonth component

--- src/pages/createMonth/index.tsx
+++ src/pages/createMonth/index.tsx
@@ -1,8 +1,12 @@
-import useCreateMonth from "../../hooks/useCreateMonth";
+import useCreateMonth from "../../hooks/month/useCreateMonth";
 
 import Stepper from "./components/Stepper";
 import Step1 from "./components/Step1";
 import Step2 from "./components/Step2";
+import { createMonth } from "../../services/monthService";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const CreateMonth = () => {
   const {
@@ -17,6 +21,37 @@ const CreateMonth = () => {
     totalAllocated,
   } = useCreateMonth();
 
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleCreateMonth = async () => {
+    if (loading) return;
+    if (salary <= 0) {
+      return toast.warning("Informe um salário válido.");
+    }
+
+    if (totalAllocated > salary) {
+      return toast.warning("Você distribuiu mais do que o salário disponível.");
+    }
+
+    if (totalAllocated < salary) {
+      return toast.warning("Distribua todo o salário antes de continuar.");
+    }
+
+    try {
+      setLoading(true);
+      await createMonth(salary, categories);
+      toast.success("Mês criado com sucesso!");
+      navigate("/home");
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <section className="m-auto flex h-full w-full max-w-100 flex-col items-center gap-4">
       <Stepper currentStep={step} onStepChange={handleStepChange} />
@@ -30,11 +65,13 @@ const CreateMonth = () => {
       {step === 2 && (
         <Step2
           salary={salary}
+          isCreatingMonth={loading}
           availableBudget={availableBudget}
           totalAllocated={totalAllocated}
           categories={categories}
           addCategory={addCategory}
           removeCategory={removeCategory}
+          onCreateMonth={handleCreateMonth}
         />
       )}
     </section>
