c180816adf7b3029f7ee55dcd8b4b1c7a0d3d008
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-24 10:20:39 -0300

    refactor(Header): remove unnecessary component

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -5,10 +5,6 @@ const Header = () => {
         <p className="text-sm text-gray-400">Fevereiro de 2026</p>
         <p className="font-bold">Olá, Diego Araújo! 👋</p>
       </div>
-
-      <div className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-blue-300 bg-blue-700 text-white">
-        <p>DA</p>
-      </div>
     </div>
   );
 };
