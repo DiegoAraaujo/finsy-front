ce367c1d3ce92f63de4f64e57a94216293b5b612
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-24 19:59:58 -0300

    feat(ExpenseItem): make component dynamic with props and delete action

--- src/pages/CategoryExpenses/components/ExpenseItem.tsx
+++ src/pages/CategoryExpenses/components/ExpenseItem.tsx
@@ -1,17 +1,46 @@
-const ExpenseItem = () => {
+import type { PaymentMethod } from "../../../types/api/expense";
+import { formatCurrency } from "../../../utils/formatCurrency";
+
+interface ExpenseItem {
+  amount: number;
+  paymentMethod: PaymentMethod;
+  description?: string;
+  onDelete: () => void;
+  isPending: boolean;
+}
+const paymentMethodLabel: Record<PaymentMethod, string> = {
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  PIX: "PIX",
+  OTHER: "Outro",
+};
+
+const ExpenseItem = ({
+  amount,
+  paymentMethod,
+  description,
+  onDelete,
+  isPending,
+}: ExpenseItem) => {
   return (
     <div className="flex justify-between rounded-t-lg border-t border-gray-300 py-4">
       <div className="flex flex-col gap-1">
         <span className="flex items-center gap-4">
-          <p className="text-lg font-bold">R$ 0.16</p>
-          <p className="text-sm font-medium text-gray-500">Cartão de Crédito</p>
+          <p className="text-lg font-semibold">{formatCurrency(amount)}</p>
+          <p className="text-sm font-medium text-gray-500">
+            {paymentMethodLabel[paymentMethod]}
+          </p>
         </span>
         <span className="flex items-center gap-4 text-sm font-medium text-gray-500">
-          <p>19 de fev.</p>
-          <p>Bar</p>
+          {description && <p>{description}</p>}
         </span>
       </div>
-      <button className="cursor-pointer text-gray-500 transition-all duration-300 hover:-translate-y-0.5 hover:text-red-600">
+      <button
+        disabled={isPending}
+        onClick={onDelete}
+        className="cursor-pointer text-gray-500 transition-all duration-300 hover:-translate-y-0.5 hover:text-red-600"
+      >
         <i className="bi bi-trash" />
       </button>
     </div>
