bd3137dc0b708c3eae0630fe875692b4ec64854e
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-24 19:59:23 -0300

    feat(CategoryExpenses): integrate data hooks and implement expenses summary

--- src/pages/CategoryExpenses/index.tsx
+++ src/pages/CategoryExpenses/index.tsx
@@ -1,33 +1,65 @@
-import PageHeaderBackground from "../../components/PageHeaderBackground";
+import { useParams } from "react-router-dom";
 import BudgetSummary from "./components/BudgetSummary";
 import ExpenseList from "./components/ExpenseList";
 import Header from "./components/Header";
 import SummaryItemCard from "./components/SummaryItemCard";
 
+import Loading from "../../components/Loading";
+import { useGetExpensesByCategoryId } from "../../hooks/expense/useGetExpensesByCategoryId";
+import { useGetCategoryById } from "../../hooks/category/useGetCategoryById";
+
 const CategoryExpenses = () => {
+  const { id } = useParams();
+
+  const { data: category, isLoading: loadingCategory } = useGetCategoryById(
+    Number(id),
+  );
+
+  const { data: expenses, isLoading: loadingExpenses } =
+    useGetExpensesByCategoryId(Number(id));
+
+  if (loadingCategory || loadingExpenses) {
+    return <Loading />;
+  }
+
+  if (!category || !expenses) return null;
+  const totalExpenses = expenses.reduce((acc, c) => c.amount + acc, 0);
+
   return (
-    <>
+    <div className="flex flex-col h-full">
       <div className="relative h-64">
-        <Header categoryName="Alimetação" status="ACIMA" />
+        <Header categoryName={category.name} status="ACIMA" />
         <BudgetSummary />
       </div>
-      <div className="flex justify-around gap-4 border-b border-gray-200 py-2 relative z-10">
-        <SummaryItemCard label="Reservado para esta categoria" value={20} textColor="text-gray-900" />
-        <SummaryItemCard label="Gastos" value={53} textColor="text-gray-900" />
+      <div className="relative z-10 flex justify-around gap-4 border-b border-gray-200 py-2">
+        <SummaryItemCard
+          label="Reservado para esta categoria"
+          value={category.spendingLimit}
+          textColor="text-gray-900"
+        />
+        <SummaryItemCard
+          label="Número de gastos"
+          value={expenses.length}
+          textColor="text-gray-900"
+        />
         <SummaryItemCard
           label="Saldo"
-          value={31}
-          textColor={5 - 2 >= 0 ? "text-emerald-600" : "text-red-600"}
+          value={category.spendingLimit - totalExpenses}
+          textColor={
+            category.spendingLimit >= totalExpenses
+              ? "text-emerald-600"
+              : "text-red-600"
+          }
         />
       </div>
 
-      <div className="flex flex-col gap-2 p-4">
-        <ExpenseList />
+      <div className="flex flex-col gap-2 p-4 flex-1">
+        <ExpenseList expenses={expenses} categoryId={Number(id)} />
         <button className="w-full rounded-2xl bg-blue-600 px-4 py-2 font-medium text-white">
           <i className="bi bi-plus" /> Registrar Gasto
         </button>
       </div>
-    </>
+    </div>
   );
 };
 
