e8ce4d982b121af3cbf06d8f30051cd490cb63df
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-24 20:00:29 -0300

    feat(ExpenseList): integrate delete hook and render dynamic expenses list

--- src/pages/CategoryExpenses/components/ExpenseList.tsx
+++ src/pages/CategoryExpenses/components/ExpenseList.tsx
@@ -1,13 +1,34 @@
+import type { Expense } from "../../../types/api/expense";
 import ExpenseItem from "./ExpenseItem";
+import { useDeleteExpense } from "../../../hooks/expense/useDeleteExpense";
 
-const ExpenseList = () => {
+interface ExpenseListProps {
+  expenses: Expense[];
+  categoryId: number;
+}
+
+const ExpenseList = ({ expenses, categoryId }: ExpenseListProps) => {
+  const { mutate: deleteExpense, isPending } = useDeleteExpense(categoryId);
+
   return (
-    <div className="z-10 flex flex-col gap-4">
-      <p className="text-sm font-bold text-gray-500">Gastos (5)</p>
+    <div className="z-10 flex flex-col gap-4 flex-1">
+      <p className="text-sm font-bold text-gray-500">
+        Gastos ({expenses.length})
+      </p>
+
       <div className="flex flex-col gap-2">
-        <ExpenseItem />
-        <ExpenseItem />
-        <ExpenseItem />
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              amount={expense.amount}
+              description={expense.description}
+              paymentMethod={expense.paymentMethod}
+              onDelete={() => deleteExpense(expense.id)}
+              isPending={isPending}
+            />
+          );
+        })}
       </div>
     </div>
   );
