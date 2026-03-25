e822c9e3d2328fc3237e317626cf649c516a1e1d
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-25 07:29:39 -0300

    refactor(app): reorganize routes with layouts separation

--- src/App.tsx
+++ src/App.tsx
@@ -5,81 +5,43 @@ import Home from "./pages/home";
 import BudgetPlanningPage from "./pages/budgetPlanningPage";
 import Historic from "./pages/historic";
 import CategoryExpenses from "./pages/CategoryExpenses";
-import Container from "./components/Container";
 import Login from "./pages/login";
 import { UserContextProvider } from "./contexts/UserContext";
-import ProtectedLayout from "./components/ProtectedLayout";
 import AppGate from "./pages/appGate";
 import CreateMonth from "./pages/createMonth";
 import MonthGate from "./pages/monthGate";
+
+import FlowLayout from "./components/FlowLayout";
+import AppLayout from "./components/AppLayout";
+import PublicLayout from "./components/PublicLayout";
+import ProtectedLayout from "./components/ProtectedLayout";
 
 const App = () => {
   return (
     <UserContextProvider>
       <BrowserRouter>
         <Toaster richColors position="top-right" closeButton duration={3000} />
-        <Container>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/"
-              element={
-                <ProtectedLayout>
-                  <AppGate />
-                </ProtectedLayout>
-              }
-            />
-            <Route
-              path="/create-month"
-              element={
-                <ProtectedLayout>
-                  <CreateMonth />
-                </ProtectedLayout>
-              }
-            />
-            <Route
-              path="/month-gate"
-              element={
-                <ProtectedLayout>
-                  <MonthGate />
-                </ProtectedLayout>
-              }
-            />
-            <Route
-              path="/home"
-              element={
-                <ProtectedLayout>
-                  <Home />
-                </ProtectedLayout>
-              }
-            />
 
-            <Route
-              path="/historic"
-              element={
-                <ProtectedLayout>
-                  <Historic />
-                </ProtectedLayout>
-              }
-            />
-            <Route
-              path="/budget-planning"
-              element={
-                <ProtectedLayout>
-                  <BudgetPlanningPage />
-                </ProtectedLayout>
-              }
-            />
-            <Route
-              path="/category/:id"
-              element={
-                <ProtectedLayout>
-                  <CategoryExpenses />
-                </ProtectedLayout>
-              }
-            />
-          </Routes>
-        </Container>
+        <Routes>
+          <Route element={<PublicLayout />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+
+          <Route element={<ProtectedLayout />}>
+            <Route element={<FlowLayout />}>
+              <Route path="/" element={<AppGate />} />
+              <Route path="/create-month" element={<CreateMonth />} />
+              <Route path="/month-gate" element={<MonthGate />} />
+            </Route>
+
+            <Route element={<AppLayout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/historic" element={<Historic />} />
+              <Route path="/budget-planning" element={<BudgetPlanningPage />} />
+              <Route path="/category/:id" element={<CategoryExpenses />} />
+            </Route>
+          </Route>
+        </Routes>
       </BrowserRouter>
     </UserContextProvider>
   );
