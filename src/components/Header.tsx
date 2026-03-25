622d53e4caddc6147bd90017ed12d1e3eed9e064
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-25 07:27:59 -0300

    feat(Header): add header component with greeting

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="relative z-10 flex justify-between p-4 border-b border-gray-300">
+      <div>
+        <p className="font-bold">Olá, Diego Araújo! 👋</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
