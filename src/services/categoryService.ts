074cc1f424e9626a97d6c145db6a0a540cf690fc
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-30 14:00:59 -0300

    feat(CategoryService): implement getCategoryById with error handling

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,5 +1,16 @@
-import categories from "../data/categories";
+import { AxiosError } from "axios";
+import api from "./api";
 
-export const getCategories = async () => {
-  return categories;
+export const getCategoryById = async (id: number) => {
+  try {
+    const { data } = await api.get(`/categories/${id}`);
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      const status = error.response?.status;
+
+      if (status === 404)
+        throw new Error("Não foi possível encontrar essa categoria.");
+    }
+  }
 };
