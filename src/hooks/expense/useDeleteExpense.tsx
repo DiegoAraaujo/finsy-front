d8c3bb714bab3a334d43632edbf93a78a6dbcb6d
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-30 14:01:50 -0300

    feat(useDeleteExpense): add useDeleteExpense mutation hook

---
+++ src/hooks/expense/useDeleteExpense.tsx
@@ -0,0 +1,16 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteExpense } from "../../services/expenseService";
+
+export const useDeleteExpense = (categoryId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: number) => deleteExpense(id),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["expenses", categoryId],
+      });
+    },
+  });
+};
