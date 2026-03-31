e643617a402a9bb060e46ff765b1645fde1f11d7
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-31 10:34:42 -0300

    feat(Historic): implement historic page with loading and error handling

--- src/pages/historic/index.tsx
+++ src/pages/historic/index.tsx
@@ -1,5 +1,40 @@
+import ErrorState from "../../components/ErrorState";
+import Loading from "../../components/Loading";
+import { useAllMonths } from "../../hooks/month/useAllMonths";
+import BudgetSummary from "./components/BudgetSummary";
+import MonthCard from "./components/MonthCard";
+
 const Historic = () => {
-  return <></>;
+  const { data: allMonths, isLoading, isError } = useAllMonths();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError || !allMonths) {
+    return <ErrorState message="Erro ao carregar os dados do histórico." />;
+  }
+
+  return (
+    <div className="flex h-full flex-col">
+      <div className="relative h-44">
+        <BudgetSummary />
+      </div>
+
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        {allMonths.map((month) => {
+          return (
+            <MonthCard
+              month={month.month}
+              monthId={month.id}
+              salary={month.salary}
+              year={month.year}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Historic;
