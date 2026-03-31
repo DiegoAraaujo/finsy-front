519f85952a5f5ec5025a82846ac8160536967364
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-31 10:34:53 -0300

    feat(MonthCard): add MonthCard component for displaying monthly salary details

---
+++ src/pages/historic/components/MonthCard.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { formatCurrency } from "../../../utils/formatCurrency";
+
+interface MonthCardProps {
+  monthId: number;
+  month: number;
+  year: number;
+  salary: number;
+}
+
+const MonthCard = ({ month, monthId, salary, year }: MonthCardProps) => {
+  const date = new Date(year, month - 1);
+
+  const rawMonthName = new Intl.DateTimeFormat("pt-BR", {
+    month: "long",
+  }).format(date);
+
+  const monthName =
+    rawMonthName.charAt(0).toUpperCase() + rawMonthName.slice(1);
+
+  return (
+    <Link
+      to={`/month/${monthId}`}
+      className="group flex cursor-pointer flex-col gap-4 rounded-2xl border-t border-gray-200 bg-white p-4 transition-all duration-300 hover:bg-gray-100"
+    >
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-1">
+          <p className="font-bold">
+            {monthName} de {year}
+          </p>
+          <p className="text-sm font-medium text-gray-500">
+            Salário {formatCurrency(salary)}
+          </p>
+        </div>
+        <i className="bi bi-chevron-right text-sm text-gray-500 transition-all duration-300 group-hover:translate-x-1 group-hover:text-blue-600" />
+      </div>
+    </Link>
+  );
+};
+
+export default MonthCard;
