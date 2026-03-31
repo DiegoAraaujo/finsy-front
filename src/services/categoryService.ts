0ef3e1c0c2b543914a87faccc2b528fe97bd26f8
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-31 14:32:13 -0300

    feat(categoryService): add error handling for fetching categories with expenses

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -14,3 +14,18 @@ export const getCategoryById = async (id: number) => {
     }
   }
 };
+
+export const getCategoriesByMonthWithExpenses = async (id: number) => {
+  try {
+    const { data } = await api.get(`/categories/${id}/with-expenses`);
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError && error.response?.status === 404) {
+      throw new Error("Categorias não encontradas.");
+    }
+
+    throw new Error(
+      "Ocorreu um erro ao buscar os dados. Tente novamente mais tarde.",
+    );
+  }
+};
