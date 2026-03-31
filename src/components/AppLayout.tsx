62c47a3c018f3d9921a8e005c82faab1ee9fb413
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-30 21:24:58 -0300

    refactor(AppLayout): comment out Header component

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -8,7 +8,7 @@ const AppLayout = () => {
   return (
     <main className="relative m-auto flex h-dvh w-full max-w-2xl flex-col overflow-x-hidden">
       <PageHeaderBackground />
-      <Header />
+      {/* <Header /> */}
       <div className="no-scrollbar relative z-20 h-full flex-1 overflow-y-auto p-4">
         <Outlet />
       </div>
