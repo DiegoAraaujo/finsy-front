d8c0de50700ec83621e0336fb38644520484d4e9
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-04-01 14:38:26 -0300

    feat: implement "Coming Soon" UI for Dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <p className="font-bold text-gray-500">Página em Desenvolvimento 🚧</p>
+    </div>
+  );
+};
+
+export default Dashboard;
