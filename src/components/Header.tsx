ce30e0e3ca96f7040686291fe743fb377c19c1f0
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-04-01 14:23:48 -0300

    refactor(Header): centralize Header component and remove redundant instances

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,27 @@
-const Header = () => {
+import { Link } from "react-router-dom";
+
+interface HeaderProps {
+  title: string;
+  subtitle?: string;
+  backTo: string;
+}
+
+const Header = ({ title, subtitle, backTo }: HeaderProps) => {
   return (
-    <div className="relative z-10 flex justify-between p-4 border-b border-gray-300">
-      <div>
-        <p className="font-bold">Olá, Diego Araújo! 👋</p>
-      </div>
-    </div>
+    <header className="relative z-10 flex items-center gap-4 p-4">
+      <Link
+        to={`/${backTo}`}
+        className="cursor-pointer transition-transform duration-300 hover:-translate-x-0.5"
+      >
+        <i className="bi bi-arrow-left text-xl text-gray-400" />
+      </Link>
+      <span>
+        <p className="text-xl font-bold">{title}</p>
+        {subtitle && (
+          <p className="text-sm font-bold text-gray-400">{subtitle}</p>
+        )}
+      </span>
+    </header>
   );
 };
 
