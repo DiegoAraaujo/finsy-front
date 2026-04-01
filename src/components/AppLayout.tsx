16aa77cc83d7c3d7aac04a03820bb13f5bc0a94f
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-04-01 14:25:40 -0300

    refactor(AppLayout): remove unused commented Header from AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,5 @@
 import {  Outlet } from "react-router-dom";
 import PageHeaderBackground from "./PageHeaderBackground";
-import Header from "./Header";
 import BottomBar from "./BottomBar";
 
 const AppLayout = () => {
@@ -8,7 +7,6 @@ const AppLayout = () => {
   return (
     <main className="relative m-auto flex h-dvh w-full max-w-2xl flex-col overflow-x-hidden">
       <PageHeaderBackground />
-      {/* <Header /> */}
       <div className="no-scrollbar relative z-20 h-full flex-1 overflow-y-auto p-4">
         <Outlet />
       </div>
