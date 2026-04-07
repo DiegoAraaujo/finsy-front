1010657b6f941686e3b57e2c1ea21b3863bda4c8
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-04-07 20:57:02 -0300

    fix: correct userService import casing

--- src/hooks/user/useLogout.tsx
+++ src/hooks/user/useLogout.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { logOut } from "../../services/UserService";
+import { logOut } from "../../services/userService";
 import { removeAccessToken } from "../../utils/auth";
 
 export const useLogout = () => {
