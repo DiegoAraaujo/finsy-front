c2f04f82c50442a493e5a6ef9175ab6024a52ae5
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-04-13 08:49:07 -0300

    feat: Add a message to the password reset page stating that it is in production and will be available soon.

---
+++ src/pages/resetPassword/index.tsx
@@ -0,0 +1,16 @@
+const ResetPassword = () => {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <p className="mb-2 text-xl font-bold">Reset Password 🔐</p>
+      <p className="text-secundary max-w-xs text-center">
+        Estamos preparando um sistema seguro para você redefinir sua senha e
+        proteger seus dados financeiros.
+        <span className="text-primary mt-2 block font-medium">
+          Disponível em breve!
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default ResetPassword;
